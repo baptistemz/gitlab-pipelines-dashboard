6ae4e29cbf64c2c9b45f27ec4f2e4f0d999034d2
olivier-rossiny-lmfr <olivier.rossiny@ext.leroymerlin.fr>
2019-02-06 16:20:49 +0100

    Merging

--- src/components/PipeLineListItem.js
+++ src/components/PipeLineListItem.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { ListItem, ListItemText, ListItemIcon, Icon } from '@material-ui/core';
+import { withHandlers, compose, pure } from 'recompose';
+import { ListItem, ListItemText, ListItemIcon, Icon, IconButton } from '@material-ui/core';
+import _ from 'lodash';
 
-const PipeLineListItem = ({ pipelineCalls }) => {
+const handlers = withHandlers({
+  retryPipeline: ({ project }) => (pipelineId) => async () => {
+    console.log("retrying");
+    //await axios.post(`/projects/${project.id}/pipelines/${pipelineId}/retry`);
+  },
+  cancelPipeline: ({ project }) => (pipelineId) => async () => {
+    console.log("cancel");
+    //await axios.post(`/projects/${project.id}/pipelines/${pipelineId}/cancel`);
+  }
+});
+
+const canRetry = (pipeline) => _.find(['finished', 'failed'], pipeline.status) !== null
+
+const canCancel = (pipeline) => _.find(['pending', 'running'], pipeline.status) !== null
+
+let PipeLineListItem = ({ pipelineCalls, retryPipeline, cancelPipeline }) => {
   console.log("pipelineCalls[0]", pipelineCalls[0])
   return(
     <ListItem button>
       <ListItemText primary={`ref: ${pipelineCalls[0].ref}`} />
       <ListItemText secondary={pipelineCalls[0].sha.substring(0,7)} />
-      <ListItemText><Icon className="fa fa-exclamation-circle"/></ListItemText>
       <ListItemIcon>
+        <Icon className="fa fa-exclamation-circle"/>
+        <IconButton size="small" disabled={canRetry(pipelineCalls[0])} aria-label="Retry" onClick={retryPipeline(pipelineCalls[0].id)}>
+          <Icon className="fa fa-redo"/>
+        </IconButton>
+        <IconButton size="small" disabled={canCancel(pipelineCalls[0])} aria-label="Cancel" onClick={cancelPipeline(pipelineCalls[0].id)}>
+          <Icon className="fa fa-ban"/>
+        </IconButton>
       </ListItemIcon>
     </ListItem>
   )
 }
+
+PipeLineListItem = compose(
+  handlers,
+  pure
+)(PipeLineListItem)
 
 export default PipeLineListItem
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,9 +4,6 @@ import PipeLineList from './PipeLineList';
 import { CardContent, CardActions, Card, Button, Icon, List, IconButton, Typography } from '@material-ui/core';
 import axios from 'axios';
 
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
-import DeleteIcon from '@material-ui/icons/Delete';
-
 const pipelines = withState('pipelines', 'setPipelines', {
   pipelineList: null,
   projectId: null
@@ -26,8 +23,8 @@ const handlers = withHandlers({
 const STATUS_ICONS = {
   "failed": "times",
   "finished": "check",
-};
-let ProjectCard = ({ project, pipelines, loadPipelines }) => {
+}; 
+let ProjectCard = ({ project, pipelines, loadPipelines, retryPipeline, cancelPipeline }) => {
   if(pipelines.projectId !== project.id){
     loadPipelines(project.id);
   }
@@ -39,23 +36,12 @@ let ProjectCard = ({ project, pipelines, loadPipelines }) => {
           <Icon className={"fa fa-" + STATUS_ICONS[project.import_status]}
             aria-label={project.import_status} />
         </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          {project.name_with_namespace}
-        </Typography>
         <List component="nav">
           {pipelines.pipelineList && <PipeLineList list={pipelines.pipelineList}/>}
         </List>
       </CardContent>
       <CardActions>
-        <IconButton size="small" aria-label="Retry">
-          <Icon className="fa fa-redo"/>
-        </IconButton>
-        <IconButton size="small" aria-label="Cancel">
-          <Icon className="fa fa-trash"/>
-        </IconButton>
-        <IconButton size="small" aria-label="Gitlab">
+        <IconButton size="small" href={project.web_url} aria-label="Gitlab">
           <Icon className="fab fa-gitlab"/>
         </IconButton>
       </CardActions>
